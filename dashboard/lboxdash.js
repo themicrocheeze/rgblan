9e11116d48fef4578ea048529083860e98b6e2dc
Alex <themicrocheez@gmail.com>
2023-11-27 16:28:15 -0500

    Java Script working for the lbox
    the lbox now has working replicants, the way it's set is that one script writes to the replicant while the other reads from it and writes to the on screen element. The plan will be to add these for every element on screen. As a warning these elements are not sanitized so you can casually paste raw html in them.

--- dashboard/lboxdash.js
+++ dashboard/lboxdash.js
@@ -1,6 +1,13 @@
 
 const tRepone = nodecg.Replicant('tRepone', {defaultValue: "123"});
+const matchInfoREP = nodecg.Replicant('matchInfo', {defaultValue: "Team A vs. Team B"});
 
 function reptRepone(){
     tRepone.value = document.getElementById("tone").value;
+}
+
+
+
+function setMatchInfo(){
+    matchInfoREP.value = document.getElementById("matchInfo").value;
 }
--- graphics/js/lBox.js
+++ graphics/js/lBox.js
@@ -2,9 +2,15 @@ console.log("test");
 
 
 const tRepone = nodecg.Replicant('tRepone');
+const matchInfoREP = nodecg.Replicant('matchInfo');
 
-const tReponeEL = document.getElementById("tony");
+const tReponeEL = document.getElementById("tonY");
+const matchInfoEL = document.getElementById("matchInfo");
 
 tRepone.on('change', (newVal) => {
 	tReponeEL.innerHTML = newVal;
+})
+
+matchInfoREP.on('change', (newVal) => {
+	matchInfoEL.innerHTML = newVal;
 })
